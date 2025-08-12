9d2d2e9b366ee42f40d493041cc425f297d6af21
lipfang <codeberg@lipfang.com>
2025-08-12 04:27:14 +0000

    bump version

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const version = std.SemanticVersion.parse("0.4.3") catch unreachable; // TODO: get from build.zig.zon
+const version = std.SemanticVersion.parse("0.4.4") catch unreachable; // TODO: get from build.zig.zon
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
